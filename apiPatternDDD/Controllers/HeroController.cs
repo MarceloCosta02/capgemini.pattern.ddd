0513fd69c8fdcaf097c4cb0f35ce8ab598493fb1
Marcelo Ponte <marcelo.ponte@capgemini.com>
2020-11-25 23:25:24 -0300

    Atualizados os dados da controller para Hero

---
+++ apiPatternDDD/Controllers/HeroController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using apiPatternDDD.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace apiPatternDDD.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HeroController : ControllerBase
+    {
+
+        public HeroController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult<Hero> Get()
+        {
+            return Ok();
+        }
+    }
+}
