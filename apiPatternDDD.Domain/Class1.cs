097df6b70d9a3f9a3b0703502782bed85622a0c6
Marcelo Ponte <marcelo.ponte@capgemini.com>
2020-11-25 23:23:59 -0300

    Criando entidade Hero

--- apiPatternDDD.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace apiPatternDDD.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ apiPatternDDD.Domain/Entities/Hero.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace apiPatternDDD.Domain
+{
+    public class Hero 
+    {
+        public int HeroId { get; set; }
+        public string SuperHeroName { get; set; }
+        public string RealName { get; set; }
+        public string SuperPower { get; set; }
+        public string Weapon { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Hero(int heroId, string superHeroName, string realName, string superPower, string weapon, DateTime birthDate)
+        {
+            HeroId = heroId;
+            SuperHeroName = superHeroName;
+            RealName = realName;
+            SuperPower = superPower;
+            Weapon = weapon;
+            BirthDate = birthDate;
+        }
+    }
+}
