be0ab0d96c7de5f0ae2049ea7487912182f21ac7
Marcelo Ponte <marcelo.ponte@capgemini.com>
2020-11-25 23:25:08 -0300

    Adicionando EF Core na aplicacao, o HeroContext e gerada migration

---
+++ apiPatternDDD.Infra.Data/Context/HeroContext.cs
@@ -0,0 +1,13 @@
+﻿using apiPatternDDD.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace apiPatternDDD.Infra.Data
+{
+    public class HeroContext : DbContext
+    {
+        public HeroContext(DbContextOptions<HeroContext> options) : base(options) { }
+
+        public DbSet<Hero> Hero { get; set; }     
+    }
+}
---
+++ apiPatternDDD.Infra.Data/Migrations/20201126021800_initial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace apiPatternDDD.Infra.Data.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Hero",
+                columns: table => new
+                {
+                    HeroId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SuperHeroName = table.Column<string>(nullable: true),
+                    RealName = table.Column<string>(nullable: true),
+                    SuperPower = table.Column<string>(nullable: true),
+                    Weapon = table.Column<string>(nullable: true),
+                    BirthDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hero", x => x.HeroId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Hero");
+        }
+    }
+}
